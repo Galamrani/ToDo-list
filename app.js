da48e8c4213e5d57df54d672d9713cdcb76a6b3a
Gal Amrani <galamrani2@gmail.com>
2023-08-06 19:35:33 +0300

    Add MongoDB database integration

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public")); 
 
-var items = [];
+var items = []; //to dealte
+
+mongoose.connect("mongodb://localhost:27017/todolistDB")
+
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+
+const Item = mongoose.model("Item", itemsSchema);
 
 app.get("/", function(req, res){
 
@@ -26,8 +34,10 @@ app.get("/", function(req, res){
 app.post("/", function(req, res){
 
     let item = req.body.newItem;
+   
+    saveToDB(item);
 
-    items.push(item);
+    //items.push(item);
     res.redirect("/");
 });
 
@@ -43,4 +53,13 @@ app.post("/delete", function(req, res) {
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("running on port 3000  ")
-});  
+});
+
+async function saveToDB(item) {
+    try {
+      await Item.create({ name: item });
+      console.log("Item saved successfully.");
+    } catch (err) {
+      console.error("Error saving item:", err);
+    }
+  }

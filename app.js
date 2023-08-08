653016e8f111cc04cb0b14b62108265bf9c3da1a
Gal Amrani <galamrani2@gmail.com>
2023-08-08 18:26:21 +0300

    Deploy MongoDB database

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public")); 
 
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/todolistDB', {
+// Connect to the MongoDB database
+mongoose.connect('mongodb+srv://galamrani2:6N3UYcEeQsKYhhbI@cluster0.sdpxi0e.mongodb.net/todolistDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -20,14 +20,14 @@ mongoose.connection.once('open', () => {
   console.log('Connected to Mongo todolistDB');
 });
 
-
+// Define the schema for to-do list items
 const itemsSchema = new mongoose.Schema({
   name: { type: String, required: true }
 });
+// Create a model based on the itemsSchema
 const Item = mongoose.model("Item", itemsSchema);
 
-var items = [];
-
+// Handle GET request to the root route ("/")
 app.get("/", function(req, res){
 
     let today = new Date();
@@ -41,6 +41,7 @@ app.get("/", function(req, res){
     renderItemList(req, res, day);
 });
 
+// Handle POST request to the root route ("/")
 app.post("/", function(req, res){
 
     let itemName = req.body.newItem;
@@ -49,6 +50,7 @@ app.post("/", function(req, res){
     res.redirect("/");
 });
 
+// Handle POST request to delete an item
 app.post("/delete", function(req, res) {
   const itemName = req.body.name;
   console.log(itemName);
@@ -56,12 +58,13 @@ app.post("/delete", function(req, res) {
   res.redirect("/");
 });
 
+// Start the server on the specified port
 app.listen(process.env.PORT || 3000, function(){
   const port = process.env.PORT || 3000;
   console.log("Server running on port:", port);
 });
 
-
+// Render the list view with items from the database
 async function renderItemList(req, res, day) {
   try {
     const foundItems = await Item.find({});
@@ -72,6 +75,7 @@ async function renderItemList(req, res, day) {
   }
 }
 
+// Insert a new item into the database
 async function insertItem(itemName) {
   const newItem = new Item({ name: itemName });
 
@@ -83,6 +87,7 @@ async function insertItem(itemName) {
   }
 }
 
+// Remove an item from the database
 async function removeItem(itemName) {
   try {
     const result = await Item.deleteOne({ name: itemName });

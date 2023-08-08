69dfaa5ed604c30cc45be9db2e50ad24adbf1371
Gal Amrani <galamrani2@gmail.com>
2023-08-08 17:27:15 +0300

    Integrate mongodb database

--- app.js
+++ app.js
@@ -4,19 +4,29 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
 
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public")); 
 
-var items = []; //to dealte
 
-mongoose.connect("mongodb://localhost:27017/todolistDB")
 
-const itemsSchema = new mongoose.Schema({
-    name: String
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.on('error', console.error.bind(console, 'Connection error:'));
+mongoose.connection.once('open', () => {
+  console.log('Connected to Mongo todolistDB');
 });
 
+
+const itemsSchema = new mongoose.Schema({
+  name: { type: String, required: true }
+});
 const Item = mongoose.model("Item", itemsSchema);
+
+var items = [];
 
 app.get("/", function(req, res){
 
@@ -28,38 +38,60 @@ app.get("/", function(req, res){
     };
     let day = today.toLocaleDateString("en-US",options);
 
-    res.render("list", {kindOfDay: day, newListItems: items});
+    renderItemList(req, res, day);
 });
 
 app.post("/", function(req, res){
 
-    let item = req.body.newItem;
+    let itemName = req.body.newItem;
    
-    saveToDB(item);
-
-    //items.push(item);
+    insertItem(itemName);
     res.redirect("/");
 });
 
 app.post("/delete", function(req, res) {
-    var index = req.body.index; 
-  
-    if (index >= 0 && index < items.length) {
-      items.splice(index, 1);
-    }
-  
-    res.redirect("/");
+  const itemName = req.body.name;
+  console.log(itemName);
+  removeItem(itemName)
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, function(){
-    console.log("running on port 3000  ")
+  const port = process.env.PORT || 3000;
+  console.log("Server running on port:", port);
 });
 
-async function saveToDB(item) {
-    try {
-      await Item.create({ name: item });
-      console.log("Item saved successfully.");
-    } catch (err) {
-      console.error("Error saving item:", err);
-    }
+
+async function renderItemList(req, res, day) {
+  try {
+    const foundItems = await Item.find({});
+    res.render("list", { kindOfDay: day, newListItems: foundItems });
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).send('An error occurred while fetching items.');
   }
+}
+
+async function insertItem(itemName) {
+  const newItem = new Item({ name: itemName });
+
+  try {
+    const result = await newItem.save();
+    console.log(itemName + " saved in db");
+  } catch (err) {
+    console.error("Error saving document:", err);
+  }
+}
+
+async function removeItem(itemName) {
+  try {
+    const result = await Item.deleteOne({ name: itemName });
+    if (result.deletedCount === 1) {
+      console.log('Item deleted successfully.');
+    } else {
+      console.log('Item not found or not deleted.');
+    }
+  } catch (err) {
+    console.error('Error deleting item:', err);
+  }
+}
